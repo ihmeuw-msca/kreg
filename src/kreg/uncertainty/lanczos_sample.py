441d0f8cfe3cd28265f7868cc63a22e899139367
Peng Zheng <zhengp@uw.edu>
2024-06-06 10:06:48 +0800

    adapt to the new version of the matfree=0.0.18

--- src/kreg/uncertainty/lanczos_sample.py
+++ src/kreg/uncertainty/lanczos_sample.py
@@ -1,32 +1,42 @@
-from matfree.lanczos import funm_vector_product_spd
+from typing import Callable
+
 import jax.numpy as jnp
+from matfree.decomp import tridiag_sym
+from matfree.funm import dense_funm_sym_eigh, funm_lanczos_sym
+
 
 def get_PC_inv_rootH(
-    PC_H_apply:callable,
-    rootpc_apply:callable,
-    max_order = 50,
-)-> callable:
+    PC_H_apply: Callable,
+    rootpc_apply: Callable,
+    max_order=50,
+) -> Callable:
     """Gets function to apply rootpc @ (rootpc@H@rootpc)^(-1/2)
     in order to sample from N(0,H^-1)
 
     Parameters
     ----------
-    PC_H_apply : callable
+    PC_H_apply
         function that returns rootpc@H@rootpc@x
-    rootpc_apply : callable
+    rootpc_apply
         function that returns rootpc@x
-    max_order : int, optional
+    max_order
         maximum number of lanczos steps, by default 50
 
     Returns
     -------
     Callable
         function which applies rootpc @ (rootpc@H@rootpc)^(-1/2)
+
     """
-    
-    inv_sqrt = lambda x:1/jnp.sqrt(x)
-    matfun_vec = funm_vector_product_spd(inv_sqrt, max_order, PC_H_apply)
+
+    def inv_sqrt(x):
+        return 1 / jnp.sqrt(x)
+
+    alg_tridiag_sym = tridiag_sym(max_order)
+    dense_funm = dense_funm_sym_eigh(inv_sqrt)
+    matfun_vec = funm_lanczos_sym(dense_funm, alg_tridiag_sym)
+
     def lanczos_sample(x):
-        return rootpc_apply(matfun_vec(x))
+        return rootpc_apply(matfun_vec(PC_H_apply, x))
+
     return lanczos_sample
-    
--- src/kreg/uncertainty/xdiag.py
+++ src/kreg/uncertainty/xdiag.py
@@ -1,36 +1,54 @@
+from typing import Callable
+
 import jax
 import jax.numpy as jnp
+from jax import Array
 
-def colnorm(M):
-    return M/jnp.linalg.norm(M,axis=0,keepdims = True)
 
-def diag_prod(A,B):
-    return jnp.sum(A * B,axis=0)
+def colnorm(M: Array) -> Array:
+    return M / jnp.linalg.norm(M, axis=0, keepdims=True)
 
-def xdiag(A_apply,n,m,key = jax.random.PRNGKey(10)):
-    """x diag estimator from 
+
+def diag_prod(A: Array, B: Array) -> float:
+    return jnp.sum(A * B, axis=0)
+
+
+def xdiag(
+    A_apply: Callable, n: int, m: int, key=jax.random.PRNGKey(10)
+) -> tuple[Array, list[Array]]:
+    """x diag estimator from
     "XTrace: Making the Most of Every Sample in Stochastic Trace Estimation"
     by Epperly, Tropp, Webber
 
     Parameters
     ----------
-    A_apply : Callable
+    A_apply
         Linear operator, assumed symmetric
-    m : int
+    m
         Number of test vectors
+    n
+        Number of columns of A
+    key
+        Random key, by default jax.random.PRNGKey(10)
+
+    Returns
+    -------
+    Array, List[Array]
+        xdiag estimate, list of intermediate values
+
     """
-    m = int(jnp.floor(m/2))
-    rad = jax.random.rademacher(key,(n,m),'float64')
+    m = int(jnp.floor(m / 2))
+    rad = jax.random.rademacher(key, (n, m), "float64")
     Y = A_apply(rad)
-    Q,R = jnp.linalg.qr(Y)
+    Q, R = jnp.linalg.qr(Y)
     Z = A_apply(Q)
-    T = Z.T@rad
+    T = Z.T @ rad
     S = colnorm(jnp.linalg.inv(R).T)
-    
-    d_QZ = diag_prod(Q.T,Z.T)
-    d_QSSZ = diag_prod((Q@S).T,(Z@S).T)
-    d_radQT = diag_prod(rad.T,(Q@T).T)
-    d_radY =diag_prod(rad.T,Y.T)
-    d_rad_QSST = diag_prod(rad.T,(Q@S@jnp.diag(diag_prod(S,T))).T)
-    d = d_QZ + (- d_QSSZ + d_radY - d_radQT + d_rad_QSST)/m
-    return d,[d_QZ,d_QSSZ,d_radY,d_radQT,d_rad_QSST]
+
+    d_QZ = diag_prod(Q.T, Z.T)
+    d_QSSZ = diag_prod((Q @ S).T, (Z @ S).T)
+    d_radQT = diag_prod(rad.T, (Q @ T).T)
+    d_radY = diag_prod(rad.T, Y.T)
+    d_rad_QSST = diag_prod(rad.T, (Q @ S @ jnp.diag(diag_prod(S, T))).T)
+    d = d_QZ + (-d_QSSZ + d_radY - d_radQT + d_rad_QSST) / m
+    return d, [d_QZ, d_QSSZ, d_radY, d_radQT, d_rad_QSST]
