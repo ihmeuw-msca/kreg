Added a sort to the basic example to line up with the current sorting of covariates. Also, fixed a bug in linesearch we were missing a minus sign in the armijo condition

src/kreg/solver/line_search.py
@@ -9,13 +9,13 @@ def armijo_line_search(
     g: JAXArray,
     objective: Callable,
     step_init: float = 1.0,
-    alpha: float = 0.6,
-    shrinkage: float = 0.2,
+    alpha: float = 0.1,
+    shrinkage: float = 0.5,
 ):
     step = step_init
     new_x = x - step * p
     val, new_val = objective(x), objective(new_x)
-    while new_val - val >= step * alpha * jnp.dot(g, p) or jnp.isnan(new_val):
+    while new_val - val >= -1 * step * alpha * jnp.dot(g, p) or jnp.isnan(new_val):
         if step <= 1e-15:
             raise RuntimeError(
                 f"Line Search Failed, new_val = {new_val}, prev_val = {val}"