7d90d116c756b68de8ce303b59f9479f9a8f8af7
zhengp0 <zhengp@uw.edu>
2024-10-03 10:46:18 -0700

    add alternative armijo line-search from the msca package

--- src/kreg/solver/line_search.py
+++ src/kreg/solver/line_search.py
@@ -35,3 +35,55 @@ def armijo_line_search(
     armijo_ratio = (val - new_val) / (step * jnp.dot(g, p))
 
     return step, armijo_ratio, (jnp.linalg.norm(new_grad) / jnp.linalg.norm(g))
+
+
+def armijo_line_search_other(
+    gradient: Callable,
+    x: JAXArray,
+    dx: JAXArray,
+    step_init: float = 1.0,
+    step_const: float = 0.01,
+    step_scale: float = 0.9,
+    step_lb: float = 1e-3,
+) -> float:
+    """Armijo line search.
+
+    Parameters
+    ----------
+    x
+        A list a parameters, including x, s, and v, where s is the slackness
+        variable and v is the dual variable for the constraints.
+    dx
+        A list of direction for the parameters.
+    step_init
+        Initial step size, by default 1.0.
+    step_const
+        Constant for the line search condition, the larger the harder, by
+        default 0.01.
+    step_scale
+        Shrinkage factor for step size, by default 0.9.
+    step_lb
+        Lower bound of the step size when the step size is below this bound
+        the line search will be terminated.
+
+    Returns
+    -------
+    float
+        The step size in the given direction.
+
+    """
+    step = step_init
+    x_next = x + step * dx
+    g_next = gradient(x_next)
+    gnorm_curr = jnp.max(jnp.abs(gradient(x)))
+    gnorm_next = jnp.max(jnp.abs(g_next))
+
+    while gnorm_next > (1 - step_const * step) * gnorm_curr:
+        if step * step_scale < step_lb:
+            break
+        step *= step_scale
+        x_next = x + step * dx
+        g_next = gradient(x_next)
+        gnorm_next = jnp.max(jnp.abs(g_next))
+
+    return step
