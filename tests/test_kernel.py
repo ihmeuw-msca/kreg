33cb6772e96d7df78638dd3b1087839b3a93db0f
zhengp0 <zhengp@uw.edu>
2025-11-18 14:27:54 -0800

    add some simple tests

--- tests/test_kernel.py
+++ tests/test_kernel.py
@@ -2,7 +2,11 @@ import jax.numpy as jnp
 import pandas as pd
 
 from kreg.kernel.component import KernelComponent
-from kreg.kernel.factory import build_exp_similarity_kfunc, vectorize_kfunc
+from kreg.kernel.factory import (
+    build_exp_similarity_kfunc,
+    build_matern_three_half_kfunc,
+    vectorize_kfunc,
+)
 
 
 def test_hierarchical_kernel() -> None:
@@ -34,3 +38,23 @@ def test_hierarchical_kernel() -> None:
     assert kernel_component.span.shape == (6, 3)
     kmat = kernel_component.build_kmat()
     assert kmat.shape == (6, 6)
+
+
+def test_kernel_component_normalization() -> None:
+    kfunc_1 = build_matern_three_half_kfunc(rho=5.0)
+    kfunc_2 = build_matern_three_half_kfunc(rho=10.0)
+
+    def kfunc(x, y):
+        return kfunc_1(x, y) + kfunc_2(x, y)
+
+    kernel_component = KernelComponent(["x"], vectorize_kfunc(kfunc))
+
+    data = pd.DataFrame({"x": jnp.linspace(0, 10, 11)})
+
+    kernel_component.set_span(data)
+
+    kmat = kernel_component.build_kmat(nugget=0.0, normalize=True)
+    assert jnp.allclose(jnp.diag(kmat), 1.0)
+
+    kmat = kernel_component.build_kmat(nugget=0.0, normalize=False)
+    assert jnp.allclose(jnp.diag(kmat), 2.0)
